65f4a43d7314fc753c9db9388901ab5be3ac09cb
Ihor Melnyk <igor.melnick85@gmail.com>
2023-10-20 17:18:40 +0300

    added initial files

---
+++ index.js
@@ -0,0 +1,26 @@
+function setPropertyRequired(attributeName, boolValue = true) {
+  //обов"язкове
+  var attributeProps = EdocsApi.getControlProperties(attributeName);
+  attributeProps.required = boolValue;
+  EdocsApi.setControlProperties(attributeProps);
+}
+
+function setPropertyHidden(attributeName, boolValue = true) {
+  //приховане
+  var attributeProps = EdocsApi.getControlProperties(attributeName);
+  attributeProps.hidden = boolValue;
+  EdocsApi.setControlProperties(attributeProps);
+}
+
+function setPropertyDisabled(attributeName, boolValue = true) {
+  //недоступне
+  var attributeProps = EdocsApi.getControlProperties(attributeName);
+  attributeProps.disabled = boolValue;
+  EdocsApi.setControlProperties(attributeProps);
+}
+
+function setAttrValue(attributeCode, attributeValue) {
+  var attribute = EdocsApi.getAttributeValue(attributeCode);
+  attribute.value = attributeValue;
+  EdocsApi.setAttributeValue(attribute);
+}
